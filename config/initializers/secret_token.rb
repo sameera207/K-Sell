e287703ac79571789e671ffc88771dd17127cb7a
sameera gyaan <sameera@sameera.(none)>
2012-03-24 13:59:14 +0530

    k-sell, marketplace where buyers meets sellers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KSell::Application.config.secret_token = 'd3d40b15daf24531905cf6884a697d1ed034eb261c2c7925e447675e11d590200cecbb038dc807959e047844472d9260b49733f0433fc0ce4a52546d2fe9a89a'
