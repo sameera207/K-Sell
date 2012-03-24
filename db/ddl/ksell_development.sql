4196e1dab1f8a35960a7644b573844f1cef49c8d
pradeep <ngpsanjaya@hotmail.com>
2012-03-25 00:49:26 +0530

    schema ddl

---
+++ db/ddl/ksell_development.sql
@@ -0,0 +1,173 @@
+-- MySQL dump 10.13  Distrib 5.1.58, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: ksell_development
+-- ------------------------------------------------------
+-- Server version	5.1.58-1ubuntu1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `inbox`
+--
+
+DROP TABLE IF EXISTS `inbox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inbox` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `number` varchar(20) NOT NULL DEFAULT '',
+  `smsdate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `insertdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `text` text,
+  `phone` tinyint(4) DEFAULT NULL,
+  `processed` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inbox`
+--
+
+LOCK TABLES `inbox` WRITE;
+/*!40000 ALTER TABLE `inbox` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inbox` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `msg` text,
+  `price` float DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `multipartinbox`
+--
+
+DROP TABLE IF EXISTS `multipartinbox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `multipartinbox` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `number` varchar(20) NOT NULL DEFAULT '',
+  `smsdate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `insertdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `text` text,
+  `phone` tinyint(4) DEFAULT NULL,
+  `processed` tinyint(4) NOT NULL DEFAULT '0',
+  `refnum` int(8) DEFAULT NULL,
+  `maxnum` int(8) DEFAULT NULL,
+  `curnum` int(8) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `multipartinbox`
+--
+
+LOCK TABLES `multipartinbox` WRITE;
+/*!40000 ALTER TABLE `multipartinbox` DISABLE KEYS */;
+/*!40000 ALTER TABLE `multipartinbox` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `outbox`
+--
+
+DROP TABLE IF EXISTS `outbox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `outbox` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `number` varchar(20) NOT NULL DEFAULT '',
+  `processed_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `insertdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `text` varchar(160) DEFAULT NULL,
+  `phone` tinyint(4) DEFAULT NULL,
+  `processed` tinyint(4) NOT NULL DEFAULT '0',
+  `error` tinyint(4) NOT NULL DEFAULT '-1',
+  `dreport` tinyint(4) NOT NULL DEFAULT '0',
+  `not_before` time NOT NULL DEFAULT '00:00:00',
+  `not_after` time NOT NULL DEFAULT '23:59:59',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `outbox`
+--
+
+LOCK TABLES `outbox` WRITE;
+/*!40000 ALTER TABLE `outbox` DISABLE KEYS */;
+/*!40000 ALTER TABLE `outbox` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schema_migrations`
+--
+
+DROP TABLE IF EXISTS `schema_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schema_migrations`
+--
+
+LOCK TABLES `schema_migrations` WRITE;
+/*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
+INSERT INTO `schema_migrations` VALUES ('20120324090937');
+/*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-03-25  0:48:19
