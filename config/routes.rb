4d13337f14d874d18d40a66fc79c102904ec7f49
sameera gyaan <sameera@sameera.(none)>
2012-03-25 13:47:49 +0530

    done... :)

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ KSell::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'messages#index'
+  # root :to => 'messages#index'
 
   # See how all your routes lay out with "rake routes"
 
